0732b6ea6f75e1e071e861e23fca71da5c84b7da
helmut cardenas <aleksey89col@gmail.com>
2021-11-27 13:55:57 -0600

    Adding Beer Repository

---
+++ src/main/java/com/hcardenas/brewery/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.hcardenas.brewery.repositories;
+
+import com.hcardenas.brewery.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
