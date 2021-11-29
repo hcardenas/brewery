c11702ce8592cf38982a794afc644d32f4f4b7aa
helmut cardenas <aleksey89col@gmail.com>
2021-11-28 19:16:24 -0600

    Added Mappers for Beer

---
+++ src/main/java/com/hcardenas/brewery/web/mappers/BeerMapper.java
@@ -0,0 +1,11 @@
+package com.hcardenas.brewery.web.mappers;
+
+import com.hcardenas.brewery.domain.Beer;
+import com.hcardenas.brewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    BeerDto BeerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto beer);
+}
